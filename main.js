ffcb809492adb3c35f446fc2d8f82f1e605ea89c
Logan <Logmiklos@gmail.com>
2022-03-01 20:01:41 -0500

    Add main.js and create roman numeral object

---
+++ main.js
@@ -0,0 +1,27 @@
+// Roman Numeral Converter
+
+// Convert the given number into a roman numeral.
+
+// All roman numerals answers should be provided in upper-case.
+
+function convertToRoman(num) {
+  let romanNumeral = {
+    1000: 'M',
+    900: 'CM',
+    500: 'D',
+    400: 'CD',
+    100: 'C',
+    90: 'XC',
+    50: 'L',
+    40: 'XL',
+    10: 'X',
+    9: 'IX',
+    5: 'V',
+    4: 'IV',
+    1: 'I'
+  };
+
+  return num;
+ }
+ 
+ convertToRoman(36);
